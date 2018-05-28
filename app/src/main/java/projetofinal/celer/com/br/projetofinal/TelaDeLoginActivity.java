7dbdc56f31443bd77bf1265110a467dec58052c7
regi001 <regianebaddi@gmail.com>
2018-05-28 11:14:59 -0300

    primeiro commit

---
+++ app/src/main/java/projetofinal/celer/com/br/projetofinal/TelaDeLoginActivity.java
@@ -0,0 +1,13 @@
+package projetofinal.celer.com.br.projetofinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelaDeLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_login);
+    }
+}
