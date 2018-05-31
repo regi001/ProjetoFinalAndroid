59ba087dd5f17655b2f0db95dca52cfe9c1b3441
regi001 <regianebaddi@gmail.com>
2018-05-30 22:16:44 -0300

    implementação do DAO e afins

---
+++ app/src/main/java/projetofinal/celer/com/br/projetofinal/CadastroDeGrupo/CadastroDeGrupoDAO.java
@@ -0,0 +1,107 @@
+package projetofinal.celer.com.br.projetofinal.CadastroDeGrupo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import projetofinal.celer.com.br.projetofinal.CadastroDeLogin.Cadastro;
+import projetofinal.celer.com.br.projetofinal.sqlite.BancoDados;
+
+/**
+ * Created by regia on 30/05/2018.
+ */
+
+public class CadastroDeGrupoDAO {
+
+    SQLiteDatabase db;
+
+    public CadastroDeGrupoDAO(Context context) {
+        db = BancoDados.getDB(context);
+    }
+    public void salvar(CadastroDeGrupo cadastroDeGrupo) {
+        ContentValues values = new ContentValues();
+
+        values.put(CadastroDeGrupo.NOME_DO_GRUPO,cadastroDeGrupo.getNomeDoGrupo());
+        values.put(CadastroDeGrupo.LOCAL_DE_ATUACAO,cadastroDeGrupo.getLocalDeAtuacao());
+        values.put(CadastroDeGrupo.IDADE_INICIAL_DE_ENTRADA,cadastroDeGrupo.getIdadeInicialDeEntrada());
+        values.put(CadastroDeGrupo.IDADE_FINAL_DE_SAIDA, cadastroDeGrupo.getIdadeFinalDeSaida());
+        db.insert(CadastroDeGrupo.TABELA, null, values);
+    }
+
+
+
+    public CadastroDeGrupo buscar(String id) {
+
+        String[] colunas = CadastroDeGrupo.COLUNAS;
+        String[] where = new String[]{id};
+
+        Cursor c = db.query(CadastroDeGrupo.TABELA, colunas, "_id =?", where, null, null, null, null);
+        c.moveToFirst();
+
+        CadastroDeGrupo cadastroDeGrupo = new CadastroDeGrupo();
+
+        cadastroDeGrupo.setId(Long.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.ID))));
+        cadastroDeGrupo.setNomeDoGrupo(c.getString(c.getColumnIndex(CadastroDeGrupo.NOME_DO_GRUPO)));
+        cadastroDeGrupo.setMonitorResponsavel(c.getString(c.getColumnIndex(CadastroDeGrupo.MONITOR_RESPONSAVEL)));
+        cadastroDeGrupo.setLocalDeAtuacao(c.getString(c.getColumnIndex(CadastroDeGrupo.LOCAL_DE_ATUACAO)));
+        cadastroDeGrupo.setIdadeInicialDeEntrada(Integer.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.IDADE_INICIAL_DE_ENTRADA))));
+        cadastroDeGrupo.setIdadeFinalDeSaida(Integer.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.IDADE_FINAL_DE_SAIDA))));
+
+
+       return cadastroDeGrupo;
+    }
+
+    public void alterar(CadastroDeGrupo cadastroDeGrupo) {
+
+        String[] where = new String[]{String.valueOf(cadastroDeGrupo.getId())};
+        ContentValues values = new ContentValues();
+
+        values.put(CadastroDeGrupo.NOME_DO_GRUPO,cadastroDeGrupo.getNomeDoGrupo());
+        values.put(CadastroDeGrupo.LOCAL_DE_ATUACAO,cadastroDeGrupo.getLocalDeAtuacao());
+        values.put(CadastroDeGrupo.IDADE_INICIAL_DE_ENTRADA,cadastroDeGrupo.getIdadeInicialDeEntrada());
+        values.put(CadastroDeGrupo.IDADE_FINAL_DE_SAIDA, cadastroDeGrupo.getIdadeFinalDeSaida());
+
+
+        db.update(CadastroDeGrupo.TABELA, values, "_id=?", where);
+    }
+
+    public void excluir(String id) {
+
+        String[] where = new String[]{id};
+        db.delete(CadastroDeGrupo.TABELA, "_id = ?", where);
+    }
+
+    public List<CadastroDeGrupo> listar() {
+
+        String[] colunas = CadastroDeGrupo.COLUNAS;
+        Cursor c = db.query(CadastroDeGrupo.TABELA, colunas, null, null, null, null, null);
+        List<CadastroDeGrupo> cadastroDeGrupos = new ArrayList<CadastroDeGrupo>();
+
+        if (c.moveToFirst()) ;
+        do {
+            CadastroDeGrupo cadastroDeGrupo = new CadastroDeGrupo();
+
+
+            cadastroDeGrupo.setId(Long.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.ID))));
+            cadastroDeGrupo.setNomeDoGrupo(c.getString(c.getColumnIndex(CadastroDeGrupo.NOME_DO_GRUPO)));
+            cadastroDeGrupo.setMonitorResponsavel(c.getString(c.getColumnIndex(CadastroDeGrupo.MONITOR_RESPONSAVEL)));
+            cadastroDeGrupo.setLocalDeAtuacao(c.getString(c.getColumnIndex(CadastroDeGrupo.LOCAL_DE_ATUACAO)));
+            cadastroDeGrupo.setIdadeInicialDeEntrada(Integer.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.IDADE_INICIAL_DE_ENTRADA))));
+            cadastroDeGrupo.setIdadeFinalDeSaida(Integer.valueOf(c.getString(c.getColumnIndex(CadastroDeGrupo.IDADE_FINAL_DE_SAIDA))));
+            cadastroDeGrupos.add(cadastroDeGrupo);
+
+            Log.i("lista", cadastroDeGrupo.getId() + cadastroDeGrupo.getNomeDoGrupo());
+        } while (c.moveToNext());
+        return cadastroDeGrupos;
+    }
+
+
+
+
+
+    }
--- app/src/main/java/projetofinal/celer/com/br/projetofinal/CadastroDeGrupo/TelaDeCadastroDeGrupoActivity.java
+++ app/src/main/java/projetofinal/celer/com/br/projetofinal/CadastroDeGrupo/TelaDeCadastroDeGrupoActivity.java
@@ -1,17 +1,146 @@
 package projetofinal.celer.com.br.projetofinal.CadastroDeGrupo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import projetofinal.celer.com.br.projetofinal.CadastroDeLogin.TelaDeCadastroActivity;
 import projetofinal.celer.com.br.projetofinal.R;
 
 public class TelaDeCadastroDeGrupoActivity extends Activity {
+    EditText edtNomeDoGrupo, edtMonitorResponsavel,edtId,edtIdadeDeInicio,edtIdadeFinal,edtLocalDeAtuacao;
+    TextView tvCadastrarUsuario;
+    CadastroDeGrupoDAO dao;
+
+    final int MENU_SALVAR = 1;
+    final int MENU_BUSCAR = 2;
+    final int MENU_ALTERAR = 3;
+    final int MENU_EXCLUIR = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_de_cadastro_de_grupo);
 
+        dao = new CadastroDeGrupoDAO(this);
+
+        edtId = findViewById(R.id.telaDeCadastroDeGrupo_edtId);
+        edtNomeDoGrupo = findViewById(R.id.TelaDeCadastroDeGrupo_edtNomeDoGrupo);
+        edtMonitorResponsavel = findViewById(R.id.telaDeCadastroDeGrupo_edtMonitorResponsavel);
+        edtLocalDeAtuacao = findViewById(R.id.telaDeCadastroDeGrupo_edtLocalDeAtuacao);
+        edtIdadeDeInicio = findViewById(R.id.telaDeCadastroDeGrupo_edtIdadeInicial);
+        edtIdadeFinal = findViewById(R.id.telaDeCadastroDeGrupo_edtIdadeFinal);
+
+        Intent it = getIntent();
+        Long id = it.getLongExtra(CadastroDeGrupo.ID,0);
+
+        if (id!=0){
+            edtId.setText(String.valueOf(id));
+            buscarGrupo();
+        }
+
+        tvCadastrarUsuario = findViewById(R.id.telaDeCadastroDeGrupo_tvCadastrarUsuaio);
+        tvCadastrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it;
+                it = new Intent(TelaDeCadastroDeGrupoActivity.this,TelaDeCadastroActivity.class);
+                startActivity(it);
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,MENU_SALVAR,0,R.string.menu_salvar);
+        menu.add(0,MENU_BUSCAR,0,R.string.menu_buscar);
+        menu.add(0,MENU_ALTERAR,0,R.string.menu_alterar);
+        menu.add(0,MENU_EXCLUIR,0,R.string.menu_excluir);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case MENU_SALVAR:
+                salvarGrupo();
+                break;
+
+            case MENU_BUSCAR:
+                buscarGrupo();
+                break;
+
+            case MENU_ALTERAR:
+                alterarGrupo();
+
+            case MENU_EXCLUIR:
+                excluirGrupo();
+                break;
+
+        }
+        return true;
+    }
+    public void salvarGrupo(){
+        CadastroDeGrupo cadastroDeGrupo = new CadastroDeGrupo();
+
+        cadastroDeGrupo.setNomeDoGrupo(edtNomeDoGrupo.getText().toString());
+        cadastroDeGrupo.setMonitorResponsavel(edtMonitorResponsavel.getText().toString());
+        cadastroDeGrupo.setLocalDeAtuacao(edtLocalDeAtuacao.getText().toString());
+        cadastroDeGrupo.setIdadeInicialDeEntrada(Integer.valueOf(edtIdadeDeInicio.getText().toString()));
+        cadastroDeGrupo.setIdadeFinalDeSaida(Integer.valueOf(edtIdadeFinal.getText().toString()));
+
+        dao.salvar(cadastroDeGrupo);
+        finish();
+    }
+    public void buscarGrupo(){
+
+        CadastroDeGrupo cadastroDeGrupo = dao.buscar(edtId.getText().toString());
+
+        edtNomeDoGrupo.setText(cadastroDeGrupo.getNomeDoGrupo());
+        edtMonitorResponsavel.setText(cadastroDeGrupo.getMonitorResponsavel());
+        edtLocalDeAtuacao.setText(cadastroDeGrupo.getLocalDeAtuacao());
+        edtIdadeDeInicio.setText(cadastroDeGrupo.getIdadeInicialDeEntrada());
+        edtIdadeFinal.setText(cadastroDeGrupo.getIdadeFinalDeSaida());
+
+
 
     }
+    public void alterarGrupo(){
+        CadastroDeGrupo cadastroDeGrupo = new CadastroDeGrupo();
+
+        cadastroDeGrupo.setNomeDoGrupo(edtNomeDoGrupo.getText().toString());
+        cadastroDeGrupo.setMonitorResponsavel(edtMonitorResponsavel.getText().toString());
+        cadastroDeGrupo.setLocalDeAtuacao(edtLocalDeAtuacao.getText().toString());
+        cadastroDeGrupo.setIdadeInicialDeEntrada(Integer.valueOf(edtIdadeDeInicio.getText().toString()));
+        cadastroDeGrupo.setIdadeFinalDeSaida(Integer.valueOf(edtIdadeFinal.getText().toString()));
+
+
+        dao.alterar(cadastroDeGrupo);
+        finish();
+
+    }
+    public void excluirGrupo(){
+
+        dao.excluir(edtId.getText().toString());
+        finish();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
 }
